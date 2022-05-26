27ffd8f5b5a85e895be2d2af76183ab2438609ee
fathurrohman97 <fathurdiakfari@gmail.com>
2022-05-26 22:15:58 +0700

    mengganti gambar, dan edit component ContactItemImage

--- src/data.js
+++ src/data.js
@@ -2,21 +2,21 @@ const getData = () => {
     return [
         {
             id: 1,
-            name: 'Dimas Saputra',
-            tag: 'dimasmds',
-            imageUrl: '/images/dimasmds.jpeg',
+            name: 'Hatsune Miku',
+            tag: 'hatsune39',
+            imageUrl: '/images/hatsune-miku.png',
         },
         {
             id: 2,
-            name: 'Arif Faizin',
-            tag: 'arifaizin',
-            imageUrl: '/images/arifaizin.jpeg',
+            name: 'megumin',
+            tag: 'megumin_chun2maru',
+            imageUrl: '/images/megumin.jpg',
           },
           {
             id: 3,
-            name: 'Rahmat Fajri',
-            tag: 'rfajri27',
-            imageUrl: '/images/rfajri27.jpeg',
+            name: 'Elaina',
+            tag: 'elaina_witch',
+            imageUrl: '/images/elaina.jpg',
           },
     ];
 }
